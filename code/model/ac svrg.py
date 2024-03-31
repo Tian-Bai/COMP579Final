70f915d0fcdcf35aa70dbf255b1e622297c4264e
Tian <123768900+TianB33@users.noreply.github.com>
2024-03-31 16:49:46 -0400

    update svrg. Previous version is (bad)

--- code/model/ac svrg.py
+++ code/model/ac svrg.py
@@ -10,7 +10,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.distributions import Categorical
-import svrg
 
 # Cart Pole
 
@@ -18,7 +17,7 @@ gamma = 0.95
 
 env = gym.make('CartPole-v1')
 eps = np.finfo(np.float32).eps.item()
-SavedAction = namedtuple('SavedAction', ['log_prob', 'value', 'value_snapshot'])
+SavedAction = namedtuple('SavedAction', ['log_prob', 'value'])
 
 action_dim = env.action_space.n
 state_dim  = env.observation_space.shape[0]
@@ -53,131 +52,81 @@ class Value(nn.Module):
         return value
 
 actor = Actor()
-value_snapshot = Value()
 value = Value()
 
 actor_optimizer = optim.Adam(actor.parameters(), lr=3e-2)
-
-value_optimizer_snapshot = svrg.SVRG_Snapshot(value_snapshot.parameters())
-value_optimizer = svrg.SVRG_k(value.parameters(), lr=3e-2, weight_decay=1e-4)
+value_optimizer = optim.Adam(value.parameters(), lr=3e-2)
 
 def select_action(state):
     state = torch.from_numpy(state).float()
-    prob, val, val_snapshot = actor(state), value(state), value_snapshot(state)
+    prob, val = actor(state), value(state)
 
     m = Categorical(prob)
     action = m.sample()
-    steps.append(SavedAction(m.log_prob(action), val, val_snapshot))
+    steps.append(SavedAction(m.log_prob(action), val))
 
     return action.item()
 
-def finish_episode(groupsize):
-    # now we have *groupsize* trajectories
-    # accumulate their gradients
+
+def finish_episode():
+    R = 0
+    policy_losses = [] # list to save actor (policy) loss
+    value_losses = [] # list to save critic (value) loss
+    returns = deque()
 
     global rewards, steps
 
-    for j_groupidx in range(groupsize):
-        R = 0
-        policy_losses = [] # list to save actor (policy) loss
-        value_losses = [] # list to save critic (value) loss
-        returns = deque()
+    # calculate the true value using rewards returned from the environment
+    for r in rewards[::-1]:
+        # calculate the discounted value
+        R = r + gamma * R
+        returns.appendleft(R)
 
-        # calculate the true value using rewards returned from the environment
-        for r in rewards[j_groupidx][::-1]:
-            # calculate the discounted value
-            R = r + gamma * R
-            returns.appendleft(R)
+    # normalization deleted
 
-        # normalization deleted
+    for (log_prob, value), R in zip(steps, returns):
+        advantage = R - value.item()
+        policy_losses.append(-log_prob * advantage)
+        value_losses.append(F.smooth_l1_loss(value, torch.tensor([R])))
 
-        for (log_prob, value, value_snapshot), R in zip(steps, returns):
-            advantage = R - value.item()
-            policy_losses.append(-log_prob * advantage)
-            value_losses.append(F.smooth_l1_loss(value_snapshot, torch.tensor([R])))
+    # reset gradients
+    actor_optimizer.zero_grad()
+    value_optimizer.zero_grad()
 
-        # sum up all the values of policy_losses and value_losses
-        actor_loss = torch.stack(policy_losses).sum()
-        value_loss = torch.stack(value_losses).sum()
+    # sum up all the values of policy_losses and value_losses
+    actor_loss = torch.stack(policy_losses).sum()
+    value_loss = torch.stack(value_losses).sum()
 
-        # accumulate the gradients to get the mean grad mu
-        snapshot_loss = value_loss / groupsize
-        snapshot_loss.backward(retain_graph=True)
+    # perform backprop
+    actor_loss.backward()
+    value_loss.backward()
+    actor_optimizer.step()
+    value_optimizer.step()
 
-    # pass the current optimizer parameters
-    u = value_optimizer_snapshot.get_param_groups()
-    value_optimizer.set_u(u)
-
-    for j_groupidx in range(groupsize):
-        R = 0
-        policy_losses = [] # list to save actor (policy) loss
-        value_losses = [] # list to save critic (value) loss
-        value_snapshot_losses = []
-        returns = deque()
-
-        # calculate the true value using rewards returned from the environment
-        for r in rewards[j_groupidx][::-1]:
-            # calculate the discounted value
-            R = r + gamma * R
-            returns.appendleft(R)
-
-        # normalization deleted
-
-        for (log_prob, value, value_snapshot), R in zip(steps, returns):
-            advantage = R - value.item()
-            policy_losses.append(-log_prob * advantage)
-            value_losses.append(F.smooth_l1_loss(value, torch.tensor([R])))
-            value_snapshot_losses.append(F.smooth_l1_loss(value_snapshot, torch.tensor([R])))
-
-        # sum up all the values of policy_losses and value_losses
-        actor_loss = torch.stack(policy_losses).sum()
-        value_loss = torch.stack(value_losses).sum()
-        value_snapshot_loss = torch.stack(value_snapshot_losses).sum()
-
-        value_optimizer.zero_grad()
-        value_loss.backward(retain_graph=True)
-
-        value_optimizer_snapshot.zero_grad()
-        value_snapshot_loss.backward(retain_graph=True)
-
-        value_optimizer.step(value_optimizer_snapshot.get_param_groups())
-
-        # for the actor - unmodified
-        actor_optimizer.zero_grad()
-        actor_loss.backward(retain_graph=True)
-        actor_optimizer.step()
-
-    value_optimizer_snapshot.set_param_groups(value_optimizer.get_param_groups())
-        
     rewards = []
     steps = []
 
 def main():
     ep_rewards = []
-    groupsize = 10
 
-    for i_episode in range(30):
-        for j_groupidx in range(groupsize):
-            state, _ = env.reset()
-            ep_reward = 0
+    for i_episode in range(300):
+        state, _ = env.reset()
+        ep_reward = 0
 
-            while True:
-                rewards_j = []
-                action = select_action(state)
+        while True:
+            action = select_action(state)
 
-                state, reward, term, trunc, _ = env.step(action)
-                done = term or trunc
-                rewards_j.append(reward)
-                ep_reward += reward
-                if done:
-                    rewards.append(rewards_j)
-                    break
-                ep_rewards.append(ep_reward)
-        finish_episode(groupsize)
+            state, reward, term, trunc, _ = env.step(action)
+            done = term or trunc
+            rewards.append(reward)
+            ep_reward += reward
+            if done:
+                break
+        ep_rewards.append(ep_reward)
+        finish_episode()
 
         if i_episode % 10 == 0:
             print('Episode {}\tLast reward: {:.2f}'.format(i_episode, ep_reward))
-
     clear_output(True)
     plt.figure(figsize=(20,5))
     plt.plot(ep_rewards)
